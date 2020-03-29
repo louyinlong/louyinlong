8e08894a7b411e3f52648b0a7b48a39017ed2e7a
louyinlong <1092755001@qq.com>
2020-03-29 13:44:12 +0800

    lylch0102
    Signed-off-by: louyinlong <1092755001@qq.com>

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  title: string;
+  link: string;
+  votes;
+
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'https://angular.io';
+    this.votes = 3;
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  voteUp() {
+    this.votes++;
+  }
+
+  voteDown() {
+    this.votes--;
+    if (this.votes <= 0) {
+      this.votes = 0;
+    }
+  }
+
+}
